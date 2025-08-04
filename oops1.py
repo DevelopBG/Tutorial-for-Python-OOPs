c0dff9b13231bcface0ae0a65f71c4630fc813d4
DevelopBG <bani.ghosh10@gmail.com>
2025-08-04 18:01:41 +1000

    class and object basic done

---
+++ oops1.py
@@ -0,0 +1,17 @@
+class employee:
+    # special method/magic method/dunder method - constructor
+    def __init__(self):
+        self.id = 123
+        self.salary = 50000 
+        self.designation = "SDE"
+    
+    def travel(self, destination):
+        print(f"employee is now traveling to {destination}")
+
+
+# creating of an object or instance
+sam = employee()
+destination = 'europe'
+travel_plan = sam.travel(destination)
+# print(sam.salary)
+print(type(sam))
---
+++ rough.py
@@ -0,0 +1,7 @@
+list = [1,3,5]
+string = "mlops playlist"
+myint = 123
+
+print(type(list))
+print(type(string))
+print(type(myint))
